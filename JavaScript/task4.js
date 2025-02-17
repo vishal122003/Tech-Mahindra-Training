dca72629a09ba49c9901ad99239f97bec9c7ddc9
vishal122003 <146356216+vishal122003@users.noreply.github.com>
2025-02-17 07:53:43 +0530

    Create task4.js

---
+++ JavaScript/task4.js
@@ -0,0 +1,8 @@
+mul();
+function mul() {
+    let num1=prompt("Enter the first number");
+    let num2=prompt("Enter the second number");
+    for (let index = 1; index <=num2; index++) {
+        console.log(num1*index);
+    }
+}
