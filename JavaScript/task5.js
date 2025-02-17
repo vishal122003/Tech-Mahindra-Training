c0bdd03911b59a8da63968fc203544d563e1bbe4
vishal122003 <146356216+vishal122003@users.noreply.github.com>
2025-02-17 07:55:19 +0530

    Create task5.js

---
+++ JavaScript/task5.js
@@ -0,0 +1,11 @@
+numm();  
+function numm() {
+    var num1=prompt("Enter the first number");
+    var num2=prompt("Enter the second number");
+    var num3=num2+40;
+    var i=num2;
+    while (i<=num3) {
+        console.log(num1*i);
+        i++;
+    }
+}
