80bd83443ea52a575e68001f30cc9fd2e997aab3
vishal122003 <146356216+vishal122003@users.noreply.github.com>
2025-02-17 09:37:37 +0530

    Create task1.js

---
+++ JavaScript(17-02-25)/task1.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <script src="Ex3.js"></script> -->
+</head>
+<script>
+    function name1() {
+    let num1=prompt("Enter number")
+    let num2=prompt("Enter limit")
+    for (let index = 1; index <=num2; index++) {
+        console.log(num1*index);
+    }
+}   
+name1();
+</script>
+</body>
+</html>
