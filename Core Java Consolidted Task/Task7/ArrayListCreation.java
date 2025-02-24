38524849adc33876f46fff6fbef8f2a481ad883a
vishal122003 <146356216+vishal122003@users.noreply.github.com>
2025-02-24 21:08:56 +0530

    Create ArrayListCreation.java

---
+++ Core Java Consolidted Task/Task7/ArrayListCreation.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class ArrayListCreation {
+public static void main(String[] args) {
+	ArrayList<String> al=new ArrayList<>();
+	al.add("red");
+	al.add("blue");
+	al.add("orange");
+	System.out.println(al);
+}
+}
+
+//Output:
+//[red, blue, orange]
