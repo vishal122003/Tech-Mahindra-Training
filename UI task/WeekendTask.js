a34982ba4f629e613a08847b39b157cd720ff863
vishal122003 <146356216+vishal122003@users.noreply.github.com>
2025-03-04 18:00:13 +0530

    Add files via upload

---
+++ UI task/WeekendTask.js
@@ -0,0 +1,29 @@
+// Set the date we're counting down to
+const countDownDate = new Date("April 20, 2025 09:00:00").getTime();
+
+// Update the count down every 1 second
+const x = setInterval(function() {
+    // Get today's date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now and the countdown date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the corresponding elements
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // If the countdown is finished, display some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "The conference has started!";
+    }
+}, 1000);
